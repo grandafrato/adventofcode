efecd022116aca2e3f2adaf169ec97fbc3d2ddcb
Lachlan Wilger <lachlan@wilgerfamily.com>
2021-12-04 12:48:30 -0800

    Solve part 2 of puzzle 2.

--- puzzle_2/lib/submarine_navigator.ex
+++ puzzle_2/lib/submarine_navigator.ex
@@ -2,26 +2,56 @@ defmodule SubmarineNavigator do
   alias SubmarineNavigator.SubmarineDirection
 
   def start() do
-    {:ok, x_plane} = SubmarineDirection.start_link()
-    {:ok, y_plane} = SubmarineDirection.start_link()
+    {:ok, horizontal} = SubmarineDirection.start_link()
+    {:ok, depth} = SubmarineDirection.start_link()
 
     Application.app_dir(:submarine, "priv/puzzle_input.txt")
     |> File.stream!()
     |> Enum.map(fn x ->
       case String.split(x) do
         ["forward", amount] ->
-          :ok = SubmarineDirection.increase(x_plane, String.to_integer(amount))
+          :ok = SubmarineDirection.increase(horizontal, String.to_integer(amount))
 
         ["down", amount] ->
-          :ok = SubmarineDirection.increase(y_plane, String.to_integer(amount))
+          :ok = SubmarineDirection.increase(depth, String.to_integer(amount))
 
         ["up", amount] ->
-          :ok = SubmarineDirection.decrease(y_plane, String.to_integer(amount))
+          :ok = SubmarineDirection.decrease(depth, String.to_integer(amount))
       end
     end)
 
-    answer = SubmarineDirection.value(x_plane) * SubmarineDirection.value(y_plane)
+    answer = SubmarineDirection.value(horizontal) * SubmarineDirection.value(depth)
 
     IO.puts("The submarine depth times the horizontal position is: #{answer}")
+  end
+
+  def start2() do
+    {:ok, horizontal} = SubmarineDirection.start_link()
+    {:ok, depth} = SubmarineDirection.start_link()
+    {:ok, aim} = SubmarineDirection.start_link()
+
+    Application.app_dir(:submarine, "priv/puzzle_input.txt")
+    |> File.stream!()
+    |> Enum.map(fn x ->
+      case String.split(x) do
+        ["forward", amount] ->
+          :ok = forward_part2(horizontal, depth, aim, String.to_integer(amount))
+
+        ["down", amount] ->
+          :ok = SubmarineDirection.increase(aim, String.to_integer(amount))
+
+        ["up", amount] ->
+          :ok = SubmarineDirection.decrease(aim, String.to_integer(amount))
+      end
+    end)
+
+    answer = SubmarineDirection.value(horizontal) * SubmarineDirection.value(depth)
+
+    IO.puts("The submarine depth times the horizontal position is: #{answer}")
+  end
+
+  defp forward_part2(horizontal, depth, aim, amount) do
+    :ok = SubmarineDirection.increase(horizontal, amount)
+    SubmarineDirection.increase(depth, SubmarineDirection.value(aim) * amount)
   end
 end
